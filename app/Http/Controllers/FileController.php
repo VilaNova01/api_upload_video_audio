22966eec24176807e26eb8398590135b320744ac
Yoclénio Vila Nova <166143630+VilaNova01@users.noreply.github.com>
2024-05-30 18:49:00 +0100

    Adição de novos arquivos

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FileRequest;
 use Illuminate\Http\Request;
 use App\Models\File;
+use App\Models\ConvertedAudio;
+use App\Models\Thumbnails;
 use Illuminate\Support\Facades\Storage;
 use getID3;
 
@@ -13,8 +15,10 @@ use FFMpeg\Format\Audio\Wav;
 use FFMpeg\Format\Video\X264;
 use FFMpeg\Format\Audio\Mp3;
 use FFMpeg\Coordinate\TimeCode;
+
 class FileController extends Controller
 {
+    //
     public function upload(FileRequest $request){
 
         $input=$request->validated();
@@ -22,73 +26,114 @@ class FileController extends Controller
         $file=$input['file'];
 
         $name=$file->getClientOriginalName();
-       
         $path=$file->store('files','public');
+
+        //gerar metadata
+        $getid3=new getID3();
+        $metadata=$getid3->analyze($file->getRealPath());
+        // Obter o formato do arquivo
+        $fileformat = $metadata['fileformat'] ?? 'unknown';
+        
+        // Obter a duração do arquivo
+        $duration = $metadata['playtime_seconds'] ?? 0;
+
+        // Obter o tamanho do arquivo
+        $filesize = $metadata['filesize'] ?? 0;
 
 
         $ffmpeg = FFMpeg::create();
 
-        
-      
-      //extrair trecho
-        $inputFile = $file; // O arquivo enviado pelo usuário
-      
-        $startTime = 00; // Tempo inicial
-        $duration = 30; // Duração em segundos
+        // Gerar Thumbnail
+         // Abrir o vídeo
+         $video = $ffmpeg->open("storage/".$path);
 
-         // Carregue o arquivo de mídia
-         $media = $ffmpeg->open($inputFile->getPathname());
-         // Defina os formatos de saída (ajuste conforme necessário)
-         $format = new X264('libmp3lame'); // Para vídeo
-         $format = new Mp3(); // Para áudio
+         // Definir o tempo para capturar a miniatura (exemplo: no segundo 10)
+         $frameTime = 10;
  
-         $nomeFileExtract=uniqid();
-         $media->filters()->clip(TimeCode::fromSeconds($startTime),TimeCode::fromSeconds($duration));
-         $media->save($format, "storage/files/{$nomeFileExtract}.mp4");
+         // Capturar o quadro e salvar como imagem
+         $video->frame(TimeCode::fromSeconds($frameTime))
+               ->save("storage/files/" . time() . "_thumb.jpg");
 
-        //converter audio   
 
-        $audio = $ffmpeg->open("storage/".$path);
-        $nomeFileConvert=uniqid();
-        $audio->save(new Wav(),"storage/files/{$nomeFileConvert}.wav");
- 
 
-       File::query()->create([
-        'name'=>$name,
-        'path'=>$path,
-        'daration'=>$duration,
-        'fileformat'=>$format
-       ]);
-        
+                // Extrair trecho
+               $inputFile = $file; // O arquivo enviado pelo usuário
       
-       // dd($ffmpeg);
+               $startTime = 00; // Tempo inicial
+               $duration = 2; // Duração em segundos
+       
+                // Carregue o arquivo de mídia
+                $media = $ffmpeg->open($inputFile->getPathname());
+                // Defina os formatos de saída (ajuste conforme necessário)
+                if($fileformat=="mp4"){
+                    $format = new X264('libmp3lame'); // Para vídeo
+                }else if($fileformat=="mp3"){
+              
+                    $format = new Mp3(); // Para áudio
+                }
+        
+                // Extrair
+                $nameExtrat=uniqid();
+                $media->filters()->clip(TimeCode::fromSeconds($startTime),TimeCode::fromSeconds($duration));
+
+                if($fileformat=="mp4"){
+
+                   $media->save($format, "storage/files/{$nameExtrat}trecho.mp4");
+
+                }else if($fileformat=="mp3"){
+                    $media->save($format, "storage/files/{$nameExtrat}trecho.mp3");
+                
+                }
+                /// CONVERTER AUDIO PARA WAV
+                if($fileformat=="mp3"){
+                    $audio = $ffmpeg->open("storage/".$path);
+                    $nomeFileConvert=uniqid();
+                    $audio->save(new Wav(),"storage/files/{$nomeFileConvert}AUDIO_CONVERTIDO.wav");
+                }
+                
+                //////////////////
+                $file=File::query()->create([
+                    'name'=>$name,
+                    'path'=>$path,
+                    'daration'=>$duration,
+                    'fileformat'=>$fileformat,
+                    'filesize'=>$filesize
+                ]);
+
+                //file::find("id")->
+
+                ConvertedAudio::query()->create([
+                    'file_id'=>$file->id
+                    
+                ]);
+                Thumbnails::query()->create([
+                    'file_id'=>$file->id
+                    
+                ]);
+         dd($ffmpeg);
       //  dd($path);
+
+
     }
 
     public function show_files(){
 
-        return Storage::disk('public')->allFiles("storage/files/");
-       
+        return response()->json(File::all());
     }
+
     public function download(File $file){
 
         return Storage::disk('public')->download($file->path);
-       
     }
 
-    public function show_thumbnail(File $file){
+    public function show_thumbnail(Thumbnails $file){
 
         return Storage::disk('public')->get($file->path);
        
     }
+    public function show_converted_audio(ConvertedAudio $audio){
 
-    public function show_converted_audio(File $file){
-
-        return Storage::disk('public')->get($file->path);
+        return response()->json($audio->all());
        
     }
-
-
-   
-    //
 }
